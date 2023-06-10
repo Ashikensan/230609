8c59d3fea06fa13f1467502a608e8e3a0b5bed39
Ashikensan <kenji_ashi@yahoo.co.jp>
2023-06-10 10:27:12 +0900

    JSON形式追加

--- js/text.js
+++ js/text.js
@@ -1,1 +1,14 @@
 // alert(12)
+
+var object1 = {
+    "a":"あ",
+    "b":"い",
+    "c":"う",
+    "d":"え"
+}
+// console.dir(object1)
+console.log(object1.a)
+const json_text = JSON.stringify(object1);
+console.log("o"+json_text)
+const json = JSON.parse(json_text);
+console.log(json);
